345ca54205a789f0bea1e582491d82c291e9159b
djl132 <djl132@case.edu>
2017-01-21 11:46:40 -0800

    added Fixtures service to AlbumCtrl

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,13 +1,11 @@
 (function(){
-    function AlbumCtrl(){
+    function AlbumCtrl(Fixtures){
       
       //bind album data into collection controller so that View has access to it
-      this.albumData = albumPicasso;
+      this.albumData = Fixtures.getAlbum();
     }
-  var test = new AlbumCtrl();
-  console.log(test);
   angular
     .module('blocJams') //is this line getting the app? and then adding another controller to it for the state 'collection'?
-    .controller('AlbumCtrl', 
-                AlbumCtrl);
+    .controller('AlbumCtrl', ['Fixtures',
+                AlbumCtrl]);
 })();
