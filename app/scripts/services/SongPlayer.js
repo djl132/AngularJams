3d5bb96b7bb3c1f01fc50c24975c58f19a0a6352
djl132 <djl132@case.edu>
2017-01-22 16:36:15 -0800

    still debugging

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,30 @@
+ (function() {
+     function SongPlayer() {
+          var SongPlayer = {};
+       
+      SongPlayer.play = function(song){
+             var currentBuzzObject = new buzz.sound(song.audioUrl,
+              { formats: ['mp3'], preload: true}
+              );
+
+         currentBuzzObject.play();
+       };
+          return SongPlayer;
+     }
+ 
+     angular
+         .module('blocJams')
+         .factory('SongPlayer', SongPlayer);
+ })();
+
+(function(){
+  
+  function SongPlayer(){
+    var SongPlayer = {};
+    return SongPlayer;
+  }
+
+    angular
+          .module('blocJams')
+          .factory('SongPlayer', SongPlayer);
+})();
