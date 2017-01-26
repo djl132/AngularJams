27451566261f1d475e17e9c126690e9510f00720
djl132 <djl132@case.edu>
2017-01-26 08:38:01 -0800

    Pass Attributes to a Directive
    Add attributes, max and value, to seekbar's(directive's) View, in order
    to share info on currentsongLength and currentsongTime. Did this by
    adding a public currentTime value to SongPlayer.js, where all general
    info and functionality is declared

---
+++ dist/scripts/directives/seekBar.js
@@ -0,0 +1,77 @@
+ (function() {
+      function seekBar($document) {
+        
+        var calculatePercent = function(seekBar, event) {
+             var offsetX = event.pageX - seekBar.offset().left;
+             var seekBarWidth = seekBar.width();
+            var percent = offsetX/seekBarWidth;
+            percent = Math.max(0,percent);
+            percent = Math.min(1,percent);
+            return percent;
+        };
+
+         return {
+
+              //restrict: 'E' instructs Angular to treat this directive as an element. 
+              //replace: true instructs Angular to completely replace the <seek-bar> element with the directive's HTML template
+              //The templateUrl option specifies the path to the HTML template that the directive will use. 
+               templateUrl: '/templates/directives/seek_bar.html',
+               replace: true,
+               restrict: 'E',
+               scope: { },//gives each seek bar instance a separate MODEL.
+               link: function(scope, element, attributes) {
+                   // directive logic to return
+                 
+                 //will store and change the real-time css percentage- value of the thumb and fill of a certain seekbar
+                 //all functions changing the style
+                 scope.value = 0;
+                 scope.max = 100;
+                 
+                 var seekBar = $(element); //differentiates which seekbar
+                 
+                 //returns ngStyle css format of percentage change 
+                 var percentString = function(){
+                   var value = scope.value;
+                   var max = scope.max;
+                   var percent = value / max * 100;
+                   return percent + "%";        };
+                 
+                 //both use data-bound scope.value stuff to update css of seekbar components
+                 scope.fillStyle = 
+                   function(){ 
+                   return {width: percentString()}; };//returns object for ng-style, which takes in an object. 
+                 scope.thumbStyle = 
+                   function(){
+                   return {left: percentString()};};
+                
+                 
+                 //click handler - event info.
+                 scope.onClickSeekBar = function(event){
+                    var percent = calculatePercent(seekBar, event);
+                    scope.value = percent * scope.max; //data bound - will update view in real-time
+                 };
+                 
+                 //tracks thumb of respective
+          scope.trackThumb = function() {                       
+            $document.bind('mousemove.thumb', function(event) {//BEGINS A NEW TURN
+                   var percent = calculatePercent(seekBar, event);
+                   scope.$apply(function() {//in order to bind data in the seekBar template(tell View seekBar template about chnages)
+
+                       scope.value = percent * scope.max;
+                    });
+                  });
+
+                  $document.bind('mouseup.thumb', function() {
+                     $document.unbind('mousemove.thumb');
+                     $document.unbind('mouseup.thumb');
+                  });
+            };
+         }
+     };
+  }
+ 
+     angular
+         .module('blocJams')
+         .directive('seekBar', ['$document',seekBar]);
+ })();
+ 
