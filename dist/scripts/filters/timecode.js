683507e7165b77c9e975e949b4f5b6e258499e28
djl132 <djl132@case.edu>
2017-01-26 21:54:14 -0800

    DONE

--- dist/scripts/filters/timecode.js
+++ dist/scripts/filters/timecode.js
@@ -4,6 +4,9 @@
      function timecode() {
          return function(seconds) {
              var seconds = Number.parseFloat(seconds);
+           if (Number.isNaN(seconds)){
+             return '-:--';
+           }
              var wholeSeconds = Math.floor(seconds);
              var minutes = Math.floor(wholeSeconds / 60);
              var remainingSeconds = wholeSeconds % 60;
